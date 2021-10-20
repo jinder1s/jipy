bb59c1d90022ff451652a0dc910da96b05dca33e
Jinder <jinder1s@gmail.com>
2021-10-19 22:09:03 -0400

    Improvements on expressions

--- jipy/expr.py
+++ jipy/expr.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
-from jipy.token import Token
 
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+from jipy.token import Token
+
 @dataclass
-class Expr:
-    pass
+class Expr(ABC):
+    @abstractmethod
+    def accept(self, visitor):
+        return NotImplemented
 
 
 @dataclass
@@ -14,15 +18,52 @@ class Binary(Expr):
     operator: Token
     right: Expr
 
+    def accept(self, visitor):
+        return visitor.visit_binary_expr(self)
+
+
 @dataclass
 class Grouping(Expr):
     expression: Expr
+
+
+    def accept(self, visitor):
+        return visitor.visit_grouping_expr(self)
+
 
 @dataclass
 class Unary(Expr):
     operator: Token
     right: Expr
 
+    def accept(self, visitor):
+        return visitor.visit_unary_expr(self)
+
+
 @dataclass
 class Literal(Expr):
     value: object
+
+
+    def accept(self, visitor):
+        return visitor.visit_literal_expr(self)
+
+
+class BaseVisitor(ABC):
+
+    @abstractmethod
+    def visit_binary_expr(self, expr: Binary):
+        return NotImplemented
+
+    @abstractmethod
+    def visit_grouping_expr(self, expr: Grouping):
+        return NotImplemented
+
+    @abstractmethod
+    def visit_unary_expr(self, expr: Unary):
+        return NotImplemented
+
+
+    @abstractmethod
+    def visit_literal_expr(self, expr: Literal):
+        return NotImplemented
