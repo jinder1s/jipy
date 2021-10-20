e76e97d0dea5eaa26b5a2b854ab0b83762d7bc37
Jinder <jinder1s@gmail.com>
2021-10-19 22:09:21 -0400

    feat: wrote a very simple ast printer

---
+++ jipy/ast_printer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from typing import List
+from jipy.expr import BaseVisitor, Expr, Binary, Grouping, Unary, Literal
+
+class AstPrinter(BaseVisitor):
+    def print(self, expr: Expr):
+        return expr.accept(self)
+
+    def parenthesize(self, name: str, exprs: List[Expr]):
+        expr_repr = [expr.accept(self) for expr in exprs]
+        return f'({name} {" ".join(expr_repr)})'
+
+    def visit_binary_expr(self, expr: Binary):
+        return self.parenthesize(expr.operator.lexeme, [expr.left, expr.right])
+
+    def visit_grouping_expr(self, expr: Grouping):
+        return self.parenthesize('group', [expr.expression])
+
+    def visit_unary_expr(self, expr: Unary):
+        return self.parenthesize(expr.operator.lexeme, [expr.right])
+
+
+    def visit_literal_expr(self, expr: Literal):
+        if expr.value is not None:
+            return str(expr.value)
---
+++ tests/test_ast_printer.py
@@ -0,0 +1,20 @@
+import pytest
+from jipy.ast_printer import AstPrinter
+from jipy.token import Token
+from jipy.token_types import TokenTypes
+from jipy.expr import Binary, Unary, Grouping, Literal
+
+def test_binary_expression():
+    ast_printer = AstPrinter()
+    expr = Binary(Literal(1), Token(TokenTypes.STAR, "*", None, 1), Literal(2))
+    assert ast_printer.print(expr) == "(* 1 2)"
+
+def test_unary_expression():
+    ast_printer = AstPrinter()
+    expr = Unary(Token(TokenTypes.STAR, "*", None, 1), Literal(2))
+    assert ast_printer.print(expr) == "(* 2)"
+
+def test_complex_expression():
+    ast_printer = AstPrinter()
+    expr = Binary(Unary(Token(TokenTypes.STAR, "*", None, 1), Literal(3)), Token(TokenTypes.STAR, "*", None, 1),  Literal(2))
+    assert ast_printer.print(expr) == '(* (* 3) 2)'
